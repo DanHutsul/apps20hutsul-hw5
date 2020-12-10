ddfca099d25ace9dc40f7b802774fb426d49177d
DanHutsul <dan.hutsul@gmail.com>
2020-12-10 20:59:07 +0200

    Update code

--- src/main/java/ua/edu/ucu/stream/AsIntStream.java
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -2,69 +2,142 @@ package ua.edu.ucu.stream;
 
 import ua.edu.ucu.function.*;
 
-public class AsIntStream implements IntStream {
+import java.util.ArrayList;
 
-    private AsIntStream() {
-        // To Do
+public class AsIntStream implements IntStream {
+    private final ArrayList<Integer> itemList;
+
+    private AsIntStream(int... values) {
+        this.itemList = new ArrayList<>();
+        for (int integer : values) {
+            this.itemList.add(integer);
+        }
     }
 
     public static IntStream of(int... values) {
-        return null;
+        return new AsIntStream(values);
     }
 
     @Override
     public Double average() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty Stream");
+        }
+        int size = (int) this.count();
+        int sum = 0;
+        for (int value : this.itemList) {
+            sum += value;
+        }
+        return (double) sum/size;
     }
 
     @Override
     public Integer max() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty Stream");
+        }
+
+        int maxValue = 0;
+        for (int value : this.itemList) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        return maxValue;
     }
 
     @Override
     public Integer min() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty Stream");
+        }
+
+        int minValue = Integer.MAX_VALUE;
+        for (int value : this.itemList) {
+            if (value < minValue) {
+                minValue = value;
+            }
+        }
+
+        return minValue;
     }
 
     @Override
     public long count() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.itemList.size();
     }
 
     @Override
     public Integer sum() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty Stream");
+        }
+        int sum = 0;
+        for (int value : this.itemList) {
+            sum += value;
+        }
+        return sum;
     }
 
     @Override
     public IntStream filter(IntPredicate predicate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> filteredList = new ArrayList<>();
+        for (Integer value : this.itemList) {
+            if (predicate.test(value)) {
+                filteredList.add(value);
+            }
+        }
+        int[] outputArray = filteredList.stream().mapToInt(i -> i).toArray();
+
+        return of(outputArray);
     }
 
     @Override
     public void forEach(IntConsumer action) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Integer value : this.itemList) {
+            action.accept(value);
+        }
     }
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] mappedArray = new int[itemList.size()];
+        for (int i = 0; i < itemList.size(); i++) {
+            mappedArray[i] = mapper.apply(itemList.get(i));
+        }
+        return of(mappedArray);
     }
 
     @Override
     public IntStream flatMap(IntToIntStreamFunction func) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> flatMappedList = new ArrayList<>();
+        for (Integer integer : itemList) {
+            for (int num : func.applyAsIntStream(integer).toArray()) {
+                flatMappedList.add(num);
+            }
+        }
+        int[] outputArray = flatMappedList.stream().mapToInt(i -> i).toArray();
+
+        return of(outputArray);
     }
 
     @Override
     public int reduce(int identity, IntBinaryOperator op) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int reduced = identity;
+        for (int value: itemList) {
+            reduced = op.apply(reduced, value);
+        }
+        return reduced;
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] outputArray = new int[itemList.size()];
+        for (int i = 0; i < itemList.size(); i++) {
+            outputArray[i] = itemList.get(i);
+        }
+        return outputArray;
     }
 
 }
--- src/test/java/ua/edu/ucu/StreamAppTest.java
+++ src/test/java/ua/edu/ucu/StreamAppTest.java
@@ -10,7 +10,7 @@ import org.junit.Before;
  * @author andrii
  */
 public class StreamAppTest {
-    
+
     private IntStream intStream;
 
     @Before
@@ -18,13 +18,13 @@ public class StreamAppTest {
         int[] intArr = {-1, 0, 1, 2, 3};
         intStream = AsIntStream.of(intArr);
     }
-    
+
     @Test
     public void testStreamOperations() {
         System.out.println("streamOperations");
         int expResult = 42;
         int result = StreamApp.streamOperations(intStream);
-        assertEquals(expResult, result);        
+        assertEquals(expResult, result);
     }
 
     @Test
@@ -32,7 +32,7 @@ public class StreamAppTest {
         System.out.println("streamToArray");
         int[] expResult = {-1, 0, 1, 2, 3};
         int[] result = StreamApp.streamToArray(intStream);
-        assertArrayEquals(expResult, result);        
+        assertArrayEquals(expResult, result);
     }
 
     @Test
@@ -40,7 +40,47 @@ public class StreamAppTest {
         System.out.println("streamForEach");
         String expResult = "-10123";
         String result = StreamApp.streamForEach(intStream);
-        assertEquals(expResult, result);        
+        assertEquals(expResult, result);
     }
-    
+
+    @Test
+    public void testAverage() {
+        System.out.println("average");
+        double expResult = 1.0;
+        double result = intStream.average();
+        assertEquals(expResult, result, 0.1);
+    }
+
+    @Test
+    public void testMaximum() {
+        System.out.println("maximum");
+        int expResult = 3;
+        int result = intStream.max();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testMinimum() {
+        System.out.println("minimum");
+        int expResult = -1;
+        int result = intStream.min();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCount() {
+        System.out.println("count");
+        long expResult = 5;
+        long result = intStream.count();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSum() {
+        System.out.println("sum");
+        int expResult = 5;
+        int result = intStream.sum();
+        assertEquals(expResult, result);
+    }
+
 }
